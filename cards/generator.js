5595d7c8dae25179aa3b9b28f78ee43138c6dc12
yyxmeng <Aries02090001@gmail.com>
2026-05-25 09:48:58 +0800

    Update generator.js

--- cards/generator.js
+++ cards/generator.js
@@ -138,6 +138,51 @@ function buildTokenizer(){
     );
 }
 
+function downloadFile(
+
+    filename,
+    data
+
+){
+
+    const blob=
+    new Blob(
+
+        [
+            JSON.stringify(
+                data,
+                null,
+                2
+            )
+        ],
+
+        {
+            type:
+            'application/json'
+        }
+    );
+
+    const url=
+    URL.createObjectURL(
+        blob
+    );
+
+    const a=
+    document
+    .createElement('a');
+
+    a.href=url;
+
+    a.download=
+    filename;
+
+    a.click();
+
+    URL.revokeObjectURL(
+        url
+    );
+}
+
 function uniquePending(list){
 
     const map=new Map();
